31942bf709b9f948ebdcbf345f4a6e818d71ee7c
toshiemon18 <st12318@gmail.com>
2016-04-22 00:27:49 +0900

    Add '-t' option

--- leaving_wild.rb
+++ leaving_wild.rb
@@ -5,15 +5,27 @@ params = {}
 opt = OptionParser.new
 opt.on('-h num') {|v| params[:h] = v }
 opt.on('-m num') {|v| params[:m] = v }
+opt.on("-t num") {|v| params[:t] = v }
 opt.parse!(ARGV)
 # p params
+
+def play_get_wild(play_time)
+  mp3_file = "mp3/Get_Wild.mp3"
+  t1 = Thread.fork do 
+    system("afplay  \"#{mp3_file}\"")
+  end
+
+  sleep(play_time.to_i)
+  t1.kill
+  system("killall afplay")
+end
 
 loop do
   now = Time.now
   if now.hour.to_i == params[:h].to_i && now.min.to_i == params[:m].to_i then
-    mp3_file = "mp3/Get_Wild.mp3"
-    system("afplay  \"#{mp3_file}\"")
-    puts "\n Completed"
-    exit
+    break
   end
 end
+
+play_time = 30 if !params[:t]
+play_get_wild(play_time)
